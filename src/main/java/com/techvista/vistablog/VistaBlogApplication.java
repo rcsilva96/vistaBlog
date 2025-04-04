93cf7fac09adc01accfc09c2407fd7527f4e402e
Renato Carvalho <rsilv1822@gmail.com>
2025-04-03 23:18:12 -0300

    first commit - blank project

---
+++ src/main/java/com/techvista/vistablog/VistaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.techvista.vistablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VistaBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techvista/vistablog/VistaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techvista.vistablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VistaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
